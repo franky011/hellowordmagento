9f108a370c8224e90e47c7f6a6889d8782f0de1c
Your Name <you@example.com>
2021-11-18 09:56:55 +0700

    first commit

---
+++ Block/Index.php
@@ -0,0 +1,19 @@
+
+<?php
+
+namespace  Inchoo\Helloworld\Block;
+ 
+use Magento\Framework\View\Element\Template;
+ 
+class Index extends \Magento\Framework\View\Element\Template
+{
+    public function __construct(Template\Context $context, array $data = [])
+    {
+        parent::__construct($context, $data);
+    }
+ 
+    public function getTitle()
+    {
+        return __('HelloWorld!');
+    }
+}
---
+++ registration.php
@@ -0,0 +1,7 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Inchoo_Helloword',
+    __DIR__
+);
